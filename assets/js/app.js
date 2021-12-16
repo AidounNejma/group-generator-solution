0d66bffce9692ad3a15651741314f9ca632a3992
AidounNejma <guermachenejma@gmail.com>
2021-12-16 12:39:17 +0100

    second commit

--- assets/js/app.js
+++ assets/js/app.js
@@ -41,3 +41,31 @@ const addNameFormElt = document.getElementById('addNameForm');
 //quand on soumet le formulaire, je veux que la fonction addParticipant soit appelée
 addNameFormElt.addEventListener("submit", addParticipant);
 
+const participants = ["Grégory", "Julien", "Nicolas", "Pierre", "Paul", "Jacques", "Sofian"];
+const numberGroups = 3;
+
+//Je veux trier un tableau de nom aléatoirement 
+// La fonction map prend un tableau en entrée
+// Applique une fonction sur chaque élément du tableau 
+// Et retourne un nouveau tableau
+const sorted = participants
+    .map((participant) => ({ name : participant, sort: Math.random() }))
+    .sort((a, b) => a.sort - b.sort)
+    .map((participant) => participant.name)
+
+    //console.log(sorted)
+//Je veux générer des groupes de nom
+const groupsArr = [];
+for(let i = 0; i < numberGroups; i++){
+    groupsArr.push([]);
+}
+
+var groups = groupsArrIndex = 0;
+while(sorted.length > 0){
+    //je prends le dernier élément du tableau sorted
+    //et je l'ajoute dans le groupe correspondant à groupsArrIndex
+    groupsArr[groupsArrIndex].push(sorted.pop());
+}
+
+console.log(groupsArr)
+//Je veux afficher les groupes sur la page
